0301f9d7f7b80a10ab6b5918c42234cfa4b4915f
vithanhtu <vithanhtu21@gmail.com>
2022-11-25 10:19:44 +0700

    update pages

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Shop from "./pages/Shop";
-import Blog from "./pages/Blog";
-import Details from "./pages/Details";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import About from "./Pages/About";
+import Shop from "./Pages/Shop";
+import Blog from "./Pages/Blog";
+import Details from "./Pages/Details";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
