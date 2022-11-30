9d5a11989a6c02f7794a8b8ef81cbeac5e9145e2
thanhtu <vithanhtu21>
2022-11-30 19:33:01 +0700

    admin desgin UI

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Details from "./pages/Details";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Admin from "./auth/Admin";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
       </Routes>
       <Footer />
     </div>
